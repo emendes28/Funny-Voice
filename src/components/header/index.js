526979a3b4a1d760b6c1c0cce5b23abbe8fafd09
Evandro <emendes28>
2020-07-11 10:53:50 -0300

    melhorias layout e ajuste de voz

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,15 +1,9 @@
 import { h } from 'preact';
-import { Link } from 'preact-router/match';
 import style from './style';
 
 const Header = () => (
   <header class={style.header}>
     <h1>Voz</h1>
-    <nav>
-      <Link activeClassName={style.active} href="/">
-        Home
-      </Link>
-    </nav>
   </header>
 );
 
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -3,9 +3,10 @@ import style from './style';
 import { Icon, InlineIcon } from "@iconify/react";
 import microphone from "@iconify/icons-mdi/microphone";
 import accountTieVoiceOutline from '@iconify/icons-mdi/account-tie-voice-outline';
+import download from '@iconify/icons-mdi/download';
 import Sidebar from "../../components/sidebar"
 
-import { Layout, Card } from "preact-mdl"
+import { Layout, Card, Slider } from "preact-mdl"
 
 
 export default class Profile extends Component {
@@ -13,6 +14,8 @@ export default class Profile extends Component {
     oqueouvi: '',
     btndisable: false,
     statusEscuta: '',
+    velocidade: 1,
+    tom: 1,
   };
 
   increment = () => {
@@ -31,9 +34,11 @@ export default class Profile extends Component {
       const voice = window.speechSynthesis.getVoices();
       const thevoice = voice.filter(voice => voice.lang === 'pt-BR');
       utterThis.voice = thevoice[0];
-      utterThis.pitch = 1.2;
-      utterThis.rate = 0.6;
+      utterThis.pitch = this.state.tom;
+      utterThis.rate = this.state.velocidade;
       synth.speak(utterThis);
+      const mediaRecorder = new MediaRecorder(stream);
+
     }
   }
 
@@ -69,6 +74,18 @@ export default class Profile extends Component {
     
     
   }
+
+  setText = (e) =>{
+    this.setState({ oqueouvi: e.target.value,disabled: true });
+  }
+  
+  setVelocidade = (e) =>{
+    this.setState({ velocidade: e.target.value,disabled: true });
+  }
+  
+  setTom = (e) =>{
+    this.setState({ tom: e.target.value,disabled: true });
+  }
   // gets called when this route is navigated to
   componentDidMount() {
     // start a timer for the clock:
@@ -81,29 +98,36 @@ export default class Profile extends Component {
   }
 
   // Note: `user` comes from the URL, courtesy of our router
-  render({ user }, { oqueouvi, btndisable, statusEscuta }) {
+  render({ user }, { oqueouvi, btndisable, velocidade, tom }) {
     return (
       
-      <Layout fixed-header fixed-drawer>
-      <Sidebar />
       <Card shadow="4" class="full-width">
         <Card.Title class="graphic">
           <Card.TitleText>Fala eu que te escuto</Card.TitleText>
         </Card.Title>
         <Card.Text style="text-align:center">
-          
-        <textarea  rows="4" cols="50">
-          {oqueouvi}
-          </textarea>
+            <fieldset> <figcaption>Mensagem</figcaption>
+            <textarea onChange={this.setText} value={oqueouvi}></textarea>
+            </fieldset>
         </Card.Text>
-        <Card.Actions style="text-align:right">
-          <Icon onClick={this.testSpeech} disabled={btndisable} icon={microphone} />
+        <Card.Actions style="text-align:center">
+          <Icon  width="15%" onClick={this.testSpeech} disabled={btndisable} icon={microphone} />        
         </Card.Actions>
-        <Card.Actions style="text-align:left">
-          <Icon onClick={this.speak} disabled={btndisable} icon={accountTieVoiceOutline} />
+        <Card.Text style="text-align:center">
+          
+        <fieldset> <figcaption>Ajuste da voz</figcaption>
+      <label for="veloz">Velocidade</label>
+            <Slider min="0.5" onChange={this.setVelocidade}  max="2" value={velocidade} step="0.1" id="veloz"/>
+      <label for="tonalidade">Tonalidade</label>
+            <Slider min="0" max="2" onChange={this.setTom}  value={tom} step="0.1" id="tonalidade"/>
+            </fieldset>
+        </Card.Text>
+        <Card.Actions style="text-align:center">
+          <Icon width="15%" onClick={this.speak} disabled={btndisable} icon={download} />
+          <Icon width="15%" onClick={this.speak} disabled={btndisable} icon={accountTieVoiceOutline} />
+        
         </Card.Actions>
       </Card>
-        </Layout>
     );
   }
 }
